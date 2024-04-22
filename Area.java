f5a5f8e4c5e692316376ae76f8cc7ee92b02878a
Lucy Auger <157738635+lauger12@users.noreply.github.com>
2024-04-22 16:11:54 -0400

    Setting up game loop

--- Area.java
+++ Area.java
@@ -1,13 +1,41 @@
+import java.util.ArrayList;
+import com.google.common.graph.*;
 
 public class Area {
     String name;
+    String desc;
+    ArrayList enemyList;
   
-    public Area(String name) {
+    public Area(String name, String desc) {
       this.name = name;
+      this.desc = desc;
+      // this.enemyList = new ArrayList<Enemy>; // haven't merged in the Enemy class yet**
+
     }
   
+
+    
+
     public String toString() {
       return name;
     }
     
+
+    // MAP BUILDER
+  public static void buildMap() {
+    // Create the area objects for our map
+    Area bass = new Area("Kelp Forest", "DESC");
+    Area mcConnell = new Area("Thermal Vents", "DESC");
+    Area burton = new Area("Final Zone", "DESC");
+    Area sabinReed = new Area("Secret Area", "DESC");
+
+    ImmutableGraph<Area> map = GraphBuilder.undirected()
+        .<Area>immutable()
+        .putEdge(bass, mcConnell)
+        .putEdge(mcConnell, sabinReed)
+        .putEdge(sabinReed, burton)
+        .build();
+
+        System.out.println(map);
+  }
   }
---
+++ MainGameLoop.java
@@ -0,0 +1,16 @@
+import com.google.common.graph.*;
+import java.util.Scanner;
+
+public class MainGameLoop {
+
+// TO DO:
+
+// DISPLAY INTRO
+
+// GET PLAYER INFO
+
+// START GAME
+
+
+
+}
