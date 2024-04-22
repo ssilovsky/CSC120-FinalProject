73979b3e842a57f353bb09077e486e284caf6378
Lucy Auger <157738635+lauger12@users.noreply.github.com>
2024-04-22 14:46:02 -0400

    Guava and map start

---
+++ Area.java
@@ -0,0 +1,13 @@
+
+public class Area {
+    String name;
+  
+    public Area(String name) {
+      this.name = name;
+    }
+  
+    public String toString() {
+      return name;
+    }
+    
+  }
---
+++ Map.java
@@ -0,0 +1,11 @@
+import com.google.common.graph.*;
+
+// public class Map {
+//     ImmutableGraph<Area> myGraph = GraphBuilder.undirected()
+//       .<Area>immutable()
+//       .putEdge()
+//       .putEdge()
+//       .putEdge()
+//       .build();
+
+// }
