dbd2638037c5afc35f17eb3676be329c7041cd27
Sophia Silovsky <ssilovsky@smith.edu>
2024-04-22 20:42:08 -0400

    finish branch

--- Enemy.java
+++ Enemy.java
@@ -1,35 +1,58 @@
 public class Enemy {
 
     private String type;
-    private int hp;
+    public int HP;
     private int defense;
     private int attack;
     private int level;
-    private int exp; 
+    private int EXP; 
 
     public Enemy(String type, int level){
         this.type = type;
         this.level = level;
         this.defense = level * 2;
         this.attack = level * 3;
-        this.hp = level * 4;
-        this.exp = level * 2;
-
+        this.HP = level * 4;
+        this.EXP = level * 2;
     }
 
-    public void takeDamage(int dmg){
-        this.hp -= dmg/this.defense;
+    private void takeDamage(int dmg){
+        if (dmg/this.defense < 1) {
+            this.HP -= 1;
+        }
+        this.HP -= dmg/this.defense;
     }
 
-    public int getAttack(){
+    private int getAttack(){
         return this.attack;
     }
 
-    public void examine(Player p ){
-        if (Player p.level > this.level, this.hp < 10) {
-            System.out.println("This " + this.type + " is pretty weak!");
-
+    private void examine(){
+        if (this.HP <1) {
+            System.out.println("The " + this.type + " is dead!");
         }
-  
+        if (this.HP == 1) {
+            System.out.println("The " + this.type + " is almost dead!");
+        } 
+        if (this.HP > 1 && this.HP <= 5) {
+            System.out.println("The " + this.type + " is looking rough.");
+        } 
+        if (this.HP > 5 ) {
+            System.out.println("The " + this.type + " is looking strong.");
+        }
+    }
+    public static void main(String[] args) {
+        Enemy e = new Enemy("Silly", 2);
+        System.out.println(e.HP);
+        e.takeDamage(1);
+        e.examine();
+        System.out.println(e.HP);
+        e.takeDamage(2);
+        e.examine();
+        System.out.println(e.HP);
+        e.takeDamage(20);
+        e.examine();
+        System.out.println(e.HP);
+
     }
 }
