d93802c568debd15a055cab43c76f432e3af7a91
digitalmug03 <mzli@smith.edu>
2024-04-22 20:40:21 -0400

    test

---
+++ Character.java
@@ -0,0 +1,145 @@
+// public class Character {
+//     /**
+//      * DEFAULT PLAYER
+//      * 
+//      */
+//     -> attributes in main playable char or make child classes? 
+
+//     private void setAttributes() {
+//         /*
+//         * Can be optimized later, if statements will do
+//         */
+//         if(this.player_class.equals("Warrior")) { /* DONE */
+//             this.hp = 26;
+//             this.mp = 4;
+//             this.strength = 18;
+//             this.agility = 4;
+//             this.resilience = 18;
+//             this.deftness = 5;
+//             this.charm = 4;
+//             this.magical_mending = 0;
+//             this.magical_might = 0;
+//         }
+//         if(this.player_class.equals("Priest")) { /* DONE */
+//             this.hp = 19;
+//             this.mp = 14;
+//             this.strength = 9;
+//             this.agility = 14;
+//             this.resilience = 9;
+//             this.deftness = 9;
+//             this.charm = 7;
+//             this.magical_mending = 18;
+//             this.magical_might = 0;
+//         }
+//         if(this.player_class.equals("Mage")) { /* DONE */
+//             this.hp = 18;
+//             this.mp = 16;
+//             this.strength = 4;
+//             this.agility = 18;
+//             this.resilience = 7;
+//             this.deftness = 14;
+//             this.charm = 7;
+//             this.magical_mending = 0;
+//             this.magical_might = 18;
+//         }
+//         if(this.player_class.equals("Martial Artist")) { /* DONE */
+//             this.hp = 24;
+//             this.mp = 2;
+//             this.strength = 18;
+//             this.agility = 23;
+//             this.resilience = 11;
+//             this.deftness = 11;
+//             this.charm = 5;
+//             this.magical_mending = 0;
+//             this.magical_might = 0;
+//         }
+//         if(this.player_class.equals("Thief")) { /* DONE */
+//             this.hp = 23;
+//             this.mp = 6;
+//             this.strength = 13;
+//             this.agility = 18;
+//             this.resilience = 11;
+//             this.deftness = 18;
+//             this.charm = 3;
+//             this.magical_mending = 4;
+//             this.magical_might = 0;
+//         }
+//         if(this.player_class.equals("Minstrel")) { /* DONE */
+//             this.hp = 20;
+//             this.mp = 6;
+//             this.strength = 9;
+//             this.agility = 8;
+//             this.resilience = 8;
+//             this.deftness = 12;
+//             this.charm = 9;
+//             this.magical_mending = 7;
+//             this.magical_might = 6;
+//         }
+//         if(this.player_class.equals("Gladiator")) { /* DONE */
+//             this.hp = 32;
+//             this.mp = 2;
+//             this.strength = 30;
+//             this.agility = 7;
+//             this.resilience = 19;
+//             this.deftness = 15;
+//             this.charm = 5;
+//             this.magical_mending = 0;
+//             this.magical_might = 0;
+//         }
+//         if(this.player_class.equals("Armamentalist")) { /* DONE */
+//             this.hp = 32;
+//             this.mp = 14;
+//             this.strength = 21;
+//             this.agility = 10;
+//             this.resilience = 15;
+//             this.deftness = 7;
+//             this.charm = 11;
+//             this.magical_mending = 0;
+//             this.magical_might = 17;
+//         }
+//         if(this.player_class.equals("Paladin")) { /* DONE */
+//             this.hp = 36;
+//             this.mp = 11;
+//             this.strength = 21;
+//             this.agility = 4;
+//             this.resilience = 22;
+//             this.deftness = 1;
+//             this.charm = 7;
+//             this.magical_mending = 10;
+//             this.magical_might = 0;
+//         }
+//         if(this.player_class.equals("Ranger")) { /* DONE */
+//             this.hp = 31;
+//             this.mp = 14;
+//             this.strength = 18;
+//             this.agility = 16;
+//             this.resilience = 14;
+//             this.deftness = 30;
+//             this.charm = 4;
+//             this.magical_mending = 12;
+//             this.magical_might = 0;
+//         }
+//         if(this.player_class.equals("Sage")) { /* DONE */
+//             this.hp = 29;
+//             this.mp = 30;
+//             this.strength = 12;
+//             this.agility = 13;
+//             this.resilience = 12;
+//             this.deftness = 3;
+//             this.charm = 14;
+//             this.magical_mending = 12;
+//             this.magical_might = 14;
+//         }
+//         if(this.player_class.equals("Luminary")) { /* DONE */
+//             this.hp = 31;
+//             this.mp = 13;
+//             this.strength = 9;
+//             this.agility = 22;
+//             this.resilience = 12;
+//             this.deftness = 16;
+//             this.charm = 18;
+//             this.magical_mending = 19;
+//             this.magical_might = 5;
+//         }
+//     }
+// }
---
+++ PlayableChar.java
@@ -0,0 +1,133 @@
+import java.util.ArrayList;
+
+public class PlayableChar {
+    private final String name;
+    private int maxHP;
+    private int HP;
+
+    private int EXP;
+    private int level;
+
+    private int attack;
+    private int defense;
+
+    private ArrayList<String> inventory;
+    
+   //Combat battleAction;
+
+    /**
+     * 
+     * @param name
+     * @param maxHP
+     * @param attack
+     * @param defense
+     */
+    public PlayableChar(String name, int maxHP, int attack, int defense){
+        this.name = name;
+        this.maxHP = 100;
+        this.HP = maxHP;
+        this.attack = attack;
+        this.defense = defense;
+        this.level = 1;
+        this.EXP = 0;
+        this.inventory = new ArrayList<String>();
+    }
+
+    /**
+     * 
+     * @param dmg
+     */
+    private void decreaseHealth(int dmg){
+        if(this.HP - dmg/this.defense < 0){
+            this.HP = 0;
+        }
+        else{
+            this.HP -= dmg/this.defense;
+        }
+    }
+
+    /**
+     * 
+     * @param amount
+     */
+    private void increaseHealth(int amount){
+        if(this.HP + amount > this.maxHP){
+            this.HP = this.maxHP;
+        }
+        else{
+            this.HP += amount;
+        }
+    }  
+
+    private void increaseEXP(int amtEXP){
+        this.EXP += amtEXP;
+        if(this.EXP >= 10) {
+            this.level += 1;
+        }
+    }
+
+    //public void examineItem(String){
+
+    //}
+
+    /**
+     * pick up method
+     * @param item
+     */
+    public void addToInventory(String item){
+        this.inventory.add(item);
+    }
+
+    /**
+     * drop item method
+     * @param item
+     */
+    public void removeFromInventory(String item){
+        this.inventory.remove(item);
+    }
+    
+    /***
+     * 10 EXP -> LVL 1
+     *  exp level*2
+     * 
+     * @return
+     */
+
+    /**
+     * accessor + mutators
+     */
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getHealth(){
+        return this.HP;
+    }   
+
+    public int getAttack(){
+        return this.attack;
+    }
+
+    public int getDefense(){
+        return this.defense;
+    }
+
+    public int getLevel(){
+        return this.level;
+    }
+
+    //inventory infinite
+
+    //public void setCombat(BattleAction chosenAction){
+    //    battleAction = chosenAction;
+    //}
+    
+}
+
+
+/**
+ * Once merged, make new branch to work on
+ */
+
+ 
