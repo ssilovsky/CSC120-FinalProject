9f59bad9b98a78822e2ebf246a0b97cb5db38174
Lucy Auger <157738635+lauger12@users.noreply.github.com>
2024-05-03 00:44:11 -0400

    JFrame start

--- Combat.java
+++ Combat.java
@@ -11,10 +11,14 @@ public class Combat {
     private Boolean battlewon = false;
     Random crit = new Random();
     private int rounds = 1;
+    JFrame combatframe = new JFrame(); // making a frame for combat
+
+
 
     // constructor for Combat class
     public Combat() {
         // this.rounds = rounds;
+
     }
 
     // Methods
@@ -25,18 +29,25 @@ public class Combat {
         System.out.println("2. Attack");
         System.out.println("3. Examine");
         System.out.println("4. Run");
+        System.out.println("Type 'help' at anytime for tips");
         System.out.println("-----------------");
         System.out.print("Input an option: ");
     }
 
     public void combatHelp() {
         System.out.println();
-        System.out.println("----------------------------------------------------------*Combat Help*-----------------------------------------------------------");
-        System.out.println("1. Battle Stance: a risk-reward attack in which you will deal massive damage, but let the enemy get multiple hits off first");
+        System.out.println(
+                "----------------------------------------------------------*Combat Help*-----------------------------------------------------------");
+        System.out.println(
+                "1. Battle Stance: a risk-reward attack in which you will deal massive damage, but let the enemy get multiple hits off first");
         System.out.println("2. Attack: basic attack against enemy");
-        System.out.println("3. Examine: examine the enemy to try to discern how much health they have left. If level is too high, you cannot get exact number");
-        System.out.println("4. Run: attempt to run from the battle. Won't always work, best for the only dire circumstances.");
-        System.out.println("----------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println(
+                "3. Examine: examine the enemy to try to discern how much health they have left. If level is too high, you cannot get exact number");
+        System.out.println(
+                "4. Run: attempt to run from the battle. Won't always work, best for the only dire circumstances.");
+        System.out.println("-'help': prints this menu");
+        System.out.println(
+                "----------------------------------------------------------------------------------------------------------------------------------");
     }
 
     /**
@@ -132,12 +143,30 @@ public class Combat {
 
     }
 
+    // public void combatGraphic() {
+    //     JFrame combatframe = new JFrame(); // making a frame for combat
+    //     combatframe.setVisible(true); // make it visible
+    //     combatframe.setSize(800, 600); // set the size of frame
+    //     combatframe.setTitle("COMBAT!");
+    //     combatframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // prevent closing window while we have combat open
+    //     combatframe.setResizable(false); // keep size constant
+    //     }
+
+
     /**
      * Combat loop that detects for specific user input and changes the values for
      * player or enemy HP accordingly
      */
     public void combatLoop(PlayableChar player, Enemy foe) {
         this.battlewon = false;
+
+        // JFrame Initialization
+        combatframe.setVisible(true); // make it visible
+        combatframe.setSize(800, 600); // set the size of frame
+        combatframe.setTitle("COMBAT!");
+        combatframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // prevent closing window while we have combat open
+        combatframe.setResizable(false); // keep size constant
+
         Random dice = new Random();
         clearConsole();
         System.out.println();
@@ -150,10 +179,9 @@ public class Combat {
             printCombatMenu();
             userInput = combatScanner.nextLine().toUpperCase();
             System.out.println();
-            if (userInput.equals("HELP")){
+            if (userInput.equals("HELP")) {
                 combatHelp();
-            }
-            else if (userInput.equals("BATTLE STANCE") || userInput.equals("1")) {
+            } else if (userInput.equals("BATTLE STANCE") || userInput.equals("1")) {
                 System.out.println("\nYou ready yourself for a powerful blow!\n");
                 enemyAttack(player, foe.getAttack());
                 // System.out.println("\nThe enemy continues to strike as you ready yourself!");
@@ -185,6 +213,8 @@ public class Combat {
                     player.healPlayer();
                     foe.healEnemy();
                     this.battlewon = false;
+                    combatframe.setVisible(false);
+                    combatframe.dispose();
                     break;
                 } else {
                     System.out.println("You weren't able to escape!");
@@ -204,6 +234,8 @@ public class Combat {
             player.healPlayer();
             this.battlewon = true;
             foe.healEnemy();
+            combatframe.setVisible(false);
+            combatframe.dispose();
         }
         if (player.getCurrentHealth() <= 0) {
             System.out.println("Battle Lost!");
